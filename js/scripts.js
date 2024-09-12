158231a85b55e5afce781972578a9a6ddc6ad7ef
vizssouza <vini.nelso@gmail.com>
2024-09-12 14:59:06 -0300

    Adicionando arquivos iniciais
    Adicionando imagens, páginas, styles e scripts.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// HAMBURGUER
+const hamburguer = document.querySelector(".hamburguer");
+const nav = document.querySelector(".nav");
+const navlist = document.querySelector(".nav-list")
+
+navlist.addEventListener("click", () => nav.classList.toggle("active"));
+hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
