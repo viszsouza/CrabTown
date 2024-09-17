96ff6ac12530ad56b1f523895f1a2731e4b1f438
vizssouza <vini.nelso@gmail.com>
2024-09-17 12:30:35 -0300

    Adicionando página de artigos
    Falta a responsividade e os links corretos

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,21 @@ const navlist = document.querySelector(".nav-list")
 
 navlist.addEventListener("click", () => nav.classList.toggle("active"));
 hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+
+// ANIMAÇÕES
+// Coletando o observador da página
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) => {
+        if (entry.isIntersecting === true){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+// Selecionando os elementos com a classe ".hidden"
+const elements = document.querySelectorAll('.hidden')
+
+// Selecionando 1 arquivos por vez da classe ".hidden"
+elements.forEach( (element) => myObserver.observe(element))
