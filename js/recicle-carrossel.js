9f83e5547159ffb3e5377fb04dc85639c9702cb9
CaioMBM <caiombmonteiro@gmail.com>
2024-09-18 18:22:09 -0300

    Corrigi o carrossel de todas as sections da pág recicle.html

--- js/recicle-carrossel.js
+++ js/recicle-carrossel.js
@@ -1,3 +1,4 @@
+// Carrossel - Metais
 function direcao(e){
     var direcao = document.getElementById("content");
 
@@ -8,5 +9,50 @@ function direcao(e){
     // Mover para direita
     else if(e == 2){
         direcao.scrollLeft = direcao.scrollLeft + 400;  // O valor tem que ser maior que a largura do card
+    }
+};
+
+
+// Carrossel - Plástico
+function carrosselPlastico(e){
+    var direcaoPlastico = document.getElementById("carrosselPlastico");
+
+    // Mover para esquerda
+    if(e == 1){
+        direcaoPlastico.scrollLeft = direcaoPlastico.scrollLeft - 400;  // O valor tem que ser maior que a largura do card
+    } 
+    // Mover para direita
+    else if(e == 2){
+        direcaoPlastico.scrollLeft = direcaoPlastico.scrollLeft + 400;  // O valor tem que ser maior que a largura do card
+    }
+};
+
+
+// Carrossel - Papéis
+function carrosselPapeis(e){
+    var direcaoPapeis = document.getElementById("carrosselPapeis");
+
+    // Mover para esquerda
+    if(e == 1){
+        direcaoPapeis.scrollLeft = direcaoPapeis.scrollLeft - 400;  // O valor tem que ser maior que a largura do card
+    } 
+    // Mover para direita
+    else if(e == 2){
+        direcaoPapeis.scrollLeft = direcaoPapeis.scrollLeft + 400;  // O valor tem que ser maior que a largura do card
+    }
+};
+
+
+// Carrossel - Vidro
+function carrosselVidro(e){
+    var direcaoVidro = document.getElementById("carrosselVidro");
+
+    // Mover para esquerda
+    if(e == 1){
+        direcaoVidro.scrollLeft = direcaoVidro.scrollLeft - 400;  // O valor tem que ser maior que a largura do card
+    } 
+    // Mover para direita
+    else if(e == 2){
+        direcaoVidro.scrollLeft = direcaoVidro.scrollLeft + 400;  // O valor tem que ser maior que a largura do card
     }
 };
