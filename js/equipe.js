5b6ae6ea2b4e91569ed70d515077408854eb2c49
CaioMBM <caiombmonteiro@gmail.com>
2024-09-19 12:18:33 -0300

    Adicionei o link do github e do linkedin da maioria dos integrantes na pág equipe.html
    - Também alterei o nome do arquivo de recicle-carrossel.js para recicle.js

---
+++ js/equipe.js
@@ -0,0 +1,148 @@
+// Card 1 - Andriw
+function andriwGithub(){
+    window.open("https://github.com/AndriwDaniel")
+}
+function andriwLinkedin(){
+    window.open("https://www.linkedin.com/in/andriw-daniel-0a222a279?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app")
+}
+
+
+// Card 2 - Jainne
+function jainneGithub(){
+    window.open("https://github.com/JainneAguiar")
+}
+function jainneLinkedin(){
+    window.open("https://www.linkedin.com/in/jainne-aguiar-376807246?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app")
+}
+
+
+// Card 3 - Mauricio
+function mauricioGithub(){
+    window.open("https://github.com/MHenrique2004")
+}
+function mauricioLinkedin(){
+    window.open("")
+}
+
+
+// Card 4 - Cristiano
+function cristianoGithub(){
+    window.open("https://github.com/CrisGaius")
+}
+function cristianoLinkedin(){
+    window.open("https://www.linkedin.com/in/cristiano-gomes-dos-santos/")
+}
+
+
+// Card 5 - João
+function joaoGithub(){
+    window.open("https://github.com/Joao-ar11")
+}
+function joaoLinkedin(){
+    window.open("https://www.linkedin.com/in/jo%C3%A3o-antonio-46305723a")
+}
+
+
+// Card 6 - Lucas
+function lucasGithub(){
+    window.open("")
+}
+function lucasLinkedin(){
+    window.open("")
+}
+
+
+// Card 7 - Paulo
+function pauloGithub(){
+    window.open("https://github.com/LockynBr")
+}
+function pauloLinkedin(){
+    window.open("https://www.linkedin.com/in/paulo-luz-dev/")
+}
+
+
+// Card 8 - Wendel
+function wendelGithub(){
+    window.open("https://github.com/wendelncols")
+}
+function wendelLinkedin(){
+    window.open("https://www.linkedin.com/in/wendel-nícolas-95220b1aa/")
+}
+
+
+// Card 9 - Aislan
+function aislanGithub(){
+    window.open("https://github.com/AislanDayvson")
+}
+function aislanLinkedin(){
+    window.open("https://www.linkedin.com/in/aislan-dayvson-573a46233/")
+}
+
+
+// Card 10 - Caio
+function caioGithub(){
+    window.open("https://github.com/CaioMBM")
+}
+function caioLinkedin(){
+    window.open("https://www.linkedin.com/in/caio-montenegro-486571250/")
+}
+
+
+// Card 11 - Vinícius
+function viniciusGithub(){
+    window.open("https://github.com/viszsouza")
+}
+function viniciusLinkedin(){
+    window.open("https://www.linkedin.com/in/vinicius-souza-307699320/")
+}
+
+
+// Card 12 - Welton
+function weltonGithub(){
+    window.open("https://github.com/WeltonKellyson")
+}
+function weltonLinkedin(){
+    window.open("https://br.linkedin.com/in/welton-kellyson-226918227?trk=people_directory")
+}
+
+
+// Card 13 - Isabela
+function isabelaLinkedin(){
+    window.open("https://www.linkedin.com/in/isabela-renali-7bb331328?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app")
+}
+
+
+// Card 14 - Jefferson
+function jeffersonGithub(){
+    window.open("")
+}
+function jeffersonLinkedin(){
+    window.open("")
+}
+
+
+// Card 15 - Guilherme
+function guilhermeGithub(){
+    window.open("https://github.com/Dyadss?tab=overview&from=2024-09-01&to=2024-09-19")
+}
+function guilhermeLinkedin(){
+    window.open("")
+}
+
+
+// Card 16 - Jorge
+function jorgeGithub(){
+    window.open("https://github.com/JorgeGoncalves203")
+}
+function jorgeLinkedin(){
+    window.open("https://www.linkedin.com/in/jorge-goncalves-a4a4b8204?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app")
+}
+
+
+// Card 17 - Nicolas
+function nicolasGithub(){
+    window.open("https://github.com/NicolasRyhan")
+}
+function nicolasLinkedin(){
+    window.open("https://www.linkedin.com/in/nicolas-ryhan-4302b3231/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app")
+}
