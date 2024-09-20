a567af0d938269fcd9ee07bf491b896ecb9fd48f
CaioMBM <caiombmonteiro@gmail.com>
2024-09-20 13:17:44 -0300

    Ajustei o quanto o card move dentro do carrossel para telas entre 767px - 992px de largura

--- js/recicle.js
+++ js/recicle.js
@@ -1,14 +1,29 @@
 // Carrossel - Metais
 function direcao(e){
     var direcao = document.getElementById("content");
+    let larguraJanela = window.innerWidth;
 
-    // Mover para esquerda
-    if(e == 1){
-        direcao.scrollLeft = direcao.scrollLeft - 400;  // O valor tem que ser maior que a largura do card
-    } 
-    // Mover para direita
-    else if(e == 2){
-        direcao.scrollLeft = direcao.scrollLeft + 400;  // O valor tem que ser maior que a largura do card
+    // Tela com (min-width: 767px) and (max-width: 992px) de largura
+    if( larguraJanela >= 767 && larguraJanela <= 992 ){
+        // Mover para esquerda
+        if(e == 1){
+            direcao.scrollLeft = direcao.scrollLeft - 300;  // O valor tem que ser maior que a largura do card
+        } 
+        // Mover para direita
+        else if(e == 2){
+            direcao.scrollLeft = direcao.scrollLeft + 300;  // O valor tem que ser maior que a largura do card
+        }
+    }
+    // Valor padrão
+    else{
+        // Mover para esquerda
+        if(e == 1){
+            direcao.scrollLeft = direcao.scrollLeft - 400;  // O valor tem que ser maior que a largura do card
+        } 
+        // Mover para direita
+        else if(e == 2){
+            direcao.scrollLeft = direcao.scrollLeft + 400;  // O valor tem que ser maior que a largura do card
+        }
     }
 };
 
@@ -16,14 +31,29 @@ function direcao(e){
 // Carrossel - Plástico
 function carrosselPlastico(e){
     var direcaoPlastico = document.getElementById("carrosselPlastico");
+    let larguraJanela = window.innerWidth;
 
-    // Mover para esquerda
-    if(e == 1){
-        direcaoPlastico.scrollLeft = direcaoPlastico.scrollLeft - 400;  // O valor tem que ser maior que a largura do card
-    } 
-    // Mover para direita
-    else if(e == 2){
-        direcaoPlastico.scrollLeft = direcaoPlastico.scrollLeft + 400;  // O valor tem que ser maior que a largura do card
+    // Tela com (min-width: 767px) and (max-width: 992px) de largura
+    if( larguraJanela >= 767 && larguraJanela <= 992 ){
+        // Mover para esquerda
+        if(e == 1){
+            direcaoPlastico.scrollLeft = direcaoPlastico.scrollLeft - 300;  // O valor tem que ser maior que a largura do card
+        } 
+        // Mover para direita
+        else if(e == 2){
+            direcaoPlastico.scrollLeft = direcaoPlastico.scrollLeft + 300;  // O valor tem que ser maior que a largura do card
+        }
+    }
+    // Valor padrão
+    else{
+        // Mover para esquerda
+        if(e == 1){
+            direcaoPlastico.scrollLeft = direcaoPlastico.scrollLeft - 400;  // O valor tem que ser maior que a largura do card
+        } 
+        // Mover para direita
+        else if(e == 2){
+            direcaoPlastico.scrollLeft = direcaoPlastico.scrollLeft + 400;  // O valor tem que ser maior que a largura do card
+        }
     }
 };
 
@@ -31,14 +61,29 @@ function carrosselPlastico(e){
 // Carrossel - Papéis
 function carrosselPapeis(e){
     var direcaoPapeis = document.getElementById("carrosselPapeis");
+    let larguraJanela = window.innerWidth;
 
-    // Mover para esquerda
-    if(e == 1){
-        direcaoPapeis.scrollLeft = direcaoPapeis.scrollLeft - 400;  // O valor tem que ser maior que a largura do card
-    } 
-    // Mover para direita
-    else if(e == 2){
-        direcaoPapeis.scrollLeft = direcaoPapeis.scrollLeft + 400;  // O valor tem que ser maior que a largura do card
+    // Tela com (min-width: 767px) and (max-width: 992px) de largura
+    if( larguraJanela >= 767 && larguraJanela <= 992 ){
+        // Mover para esquerda
+        if(e == 1){
+            direcaoPapeis.scrollLeft = direcaoPapeis.scrollLeft - 300;  // O valor tem que ser maior que a largura do card
+        } 
+        // Mover para direita
+        else if(e == 2){
+            direcaoPapeis.scrollLeft = direcaoPapeis.scrollLeft + 300;  // O valor tem que ser maior que a largura do card
+        }
+    }
+    // Valor padrão
+    else{
+        // Mover para esquerda
+        if(e == 1){
+            direcaoPapeis.scrollLeft = direcaoPapeis.scrollLeft - 400;  // O valor tem que ser maior que a largura do card
+        } 
+        // Mover para direita
+        else if(e == 2){
+            direcaoPapeis.scrollLeft = direcaoPapeis.scrollLeft + 400;  // O valor tem que ser maior que a largura do card
+        }
     }
 };
 
@@ -46,13 +91,28 @@ function carrosselPapeis(e){
 // Carrossel - Vidro
 function carrosselVidro(e){
     var direcaoVidro = document.getElementById("carrosselVidro");
+    let larguraJanela = window.innerWidth;
 
-    // Mover para esquerda
-    if(e == 1){
-        direcaoVidro.scrollLeft = direcaoVidro.scrollLeft - 400;  // O valor tem que ser maior que a largura do card
-    } 
-    // Mover para direita
-    else if(e == 2){
-        direcaoVidro.scrollLeft = direcaoVidro.scrollLeft + 400;  // O valor tem que ser maior que a largura do card
+    // Tela com (min-width: 767px) and (max-width: 767px) de largura
+    if( larguraJanela >= 767 && larguraJanela <= 992 ){
+        // Mover para esquerda
+        if(e == 1){
+            direcaoVidro.scrollLeft = direcaoVidro.scrollLeft - 300;  // O valor tem que ser maior que a largura do card
+        } 
+        // Mover para direita
+        else if(e == 2){
+            direcaoVidro.scrollLeft = direcaoVidro.scrollLeft + 300;  // O valor tem que ser maior que a largura do card
+        }
+    }
+    // Valor padrão
+    else{
+        // Mover para esquerda
+        if(e == 1){
+            direcaoVidro.scrollLeft = direcaoVidro.scrollLeft - 400;  // O valor tem que ser maior que a largura do card
+        } 
+        // Mover para direita
+        else if(e == 2){
+            direcaoVidro.scrollLeft = direcaoVidro.scrollLeft + 400;  // O valor tem que ser maior que a largura do card
+        }
     }
 };
