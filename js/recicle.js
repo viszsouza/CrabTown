89cb2dc70424256efb0bcb22557a2a29f6c923b4
CaioMBM <caiombmonteiro@gmail.com>
2024-09-22 19:15:35 -0300

    Adicionei os links nos cards da section "Metais" da pág recicle.html

--- js/recicle.js
+++ js/recicle.js
@@ -1,3 +1,4 @@
+// CARROSSEL
 // Carrossel - Metais
 function direcao(e){
     var direcao = document.getElementById("content");
@@ -70,7 +71,6 @@ function direcao(e){
         }
     }
 };
-
 
 // Carrossel - Plástico
 function carrosselPlastico(e){
@@ -145,7 +145,6 @@ function carrosselPlastico(e){
     }
 };
 
-
 // Carrossel - Papéis
 function carrosselPapeis(e){
     var direcaoPapeis = document.getElementById("carrosselPapeis");
@@ -219,7 +218,6 @@ function carrosselPapeis(e){
     }
 };
 
-
 // Carrossel - Vidro
 function carrosselVidro(e){
     var direcaoVidro = document.getElementById("carrosselVidro");
@@ -292,3 +290,38 @@ function carrosselVidro(e){
         }
     }
 };
+
+
+
+// REDIRECIONAMENTO
+// Redirecionamento - Metais
+// Card 1
+const btnMetais1 = document.getElementById("btnMetais1");
+btnMetais1.addEventListener("click", function(){
+    //window.alert("dsdsds")
+    window.open("https://youtu.be/_kd62JFOYk8?si=dblIWfCfCGYGaqNz&t=169");
+});
+
+// Card 2
+const btnMetais2 = document.getElementById("btnMetais2");
+btnMetais2.addEventListener("click", function(){
+    window.open("https://youtu.be/gT8QDibpAEs?si=AZR5K3Asmu7u9fzj&t=398");
+});
+
+// Card 3
+const btnMetais3 = document.getElementById("btnMetais3");
+btnMetais3.addEventListener("click", function(){
+    window.open("https://youtu.be/qIbHCuNtBT8?si=-rTh5GZx-1QD-3JS&t=8");
+});
+
+// Card 4
+const btnMetais4 = document.getElementById("btnMetais4");
+btnMetais4.addEventListener("click", function(){
+    window.open("https://youtu.be/qIbHCuNtBT8?si=RaawdItvkXvdOxEz&t=655");
+});
+
+// Card 5
+const btnMetais5 = document.getElementById("btnMetais5");
+btnMetais5.addEventListener("click", function(){
+    window.open("https://youtu.be/gT8QDibpAEs?si=b4zCzDydOVgID0gU&t=7");
+});
