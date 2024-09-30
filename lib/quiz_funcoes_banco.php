0fcfda99e1e3d651eb37ee04f8f2e2bd44144331
João_A.11 <jarlpereira29@gmail.com>
2024-09-30 16:37:32 -0300

    Adicionar funcoes de busca de perguntas

---
+++ lib/quiz_funcoes_banco.php
@@ -0,0 +1,96 @@
+<?php 
+
+class Pergunta {
+    private $id_pergunta;
+    private $texto;
+    private $alternativa_1;
+    private $alternativa_2;
+    private $alternativa_3;
+    private $alternativa_4;
+    private $resposta;
+
+    function __construct($id_pergunta, $texto, $alternativa_1, $alternativa_2, $alternativa_3, $alternativa_4, $resposta) {
+        $this->id_pergunta = $id_pergunta;
+        $this->texto = $texto;
+        $this->alternativa_1 = $alternativa_1;
+        $this->alternativa_2 = $alternativa_2;
+        $this->alternativa_3 = $alternativa_3;
+        $this->alternativa_4 = $alternativa_4;
+        $this->resposta = $resposta;
+    }
+
+    public function get_id_pergunta(): int {
+        return $this->id_pergunta;
+    }
+
+    public function get_texto(): string {
+        return $this->texto;
+    }
+
+    public function get_alternativa_1(): string {
+        return $this->alternativa_1;
+    }
+
+    public function get_alternativa_2(): string {
+        return $this->alternativa_2;
+    }
+
+    public function get_alternativa_3(): string {
+        return $this->alternativa_3;
+    }
+
+    public function get_alternativa_4(): string {
+        return $this->alternativa_4;
+    }
+
+    public function get_resposta(): int {
+        return $this->resposta;
+    }
+}
+
+
+function selecionar_perguntas($pdo): array {
+    $perguntas = buscar_perguntas($pdo);
+    $ids_sorteados = sortear_perguntas($perguntas);
+    $perguntas_selecionadas = [];
+
+    for ($i = 0; $i < count($ids_sorteados); $i++) {
+        $perguntas_selecionadas[] = new Pergunta(
+            $perguntas[$ids_sorteados[$i]]["id_pergunta"],
+            $perguntas[$ids_sorteados[$i]]["texto"],
+            $perguntas[$ids_sorteados[$i]]["alternativa_1"],
+            $perguntas[$ids_sorteados[$i]]["alternativa_2"],
+            $perguntas[$ids_sorteados[$i]]["alternativa_3"],
+            $perguntas[$ids_sorteados[$i]]["alternativa_4"],
+            $perguntas[$ids_sorteados[$i]]["resposta"],
+        );
+    }
+
+    return $perguntas_selecionadas;
+}
+
+function buscar_perguntas($pdo): array {
+    $sql = "SELECT * FROM perguntas";
+    $query_exec_perguntas = $pdo->prepare($sql);
+    $query_exec_perguntas->execute() or die("Erro ao buscar as perguntas");
+
+    return $query_exec_perguntas->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function sortear_perguntas($perguntas): array {
+    $total_de_perguntas = count($perguntas);
+    $numeros_sorteados = [];
+    $i = 0;
+    $max = $total_de_perguntas < 10 ? $total_de_perguntas : 10;
+
+    do {
+        $novo_int = random_int(0, $total_de_perguntas - 1);
+        
+        if (!in_array($novo_int, $numeros_sorteados)) {
+            $numeros_sorteados[] = $novo_int;
+            $i++;
+        }
+    } while ($i < $max);
+
+    return $numeros_sorteados;
+}
