d9db91daa321518ef90cfc43489ed816e9ade671
Cristiano Gomes <101535034+CrisGaius@users.noreply.github.com>
2024-10-05 15:51:01 -0300

    Adaptando dotenv para railway

--- lib/conexao.php
+++ lib/conexao.php
@@ -4,19 +4,25 @@ if(!isset($_SESSION)) {
   session_start();
 }
 
-// if(!isset($_ENV)) {
-//   $caminho_vendor = dirname(__DIR__) . "/vendor/autoload.php";
-//   require_once $caminho_vendor;
+if(file_exists(dirname(__DIR__) . "/.env")) {
+  $caminho_vendor = dirname(__DIR__) . "/vendor/autoload.php";
+  require_once $caminho_vendor;
   
-//   $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
-//   $dotenv->load();
-// }
+  $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+  $dotenv->load();
 
-$host = $_ENV['HOST'];
-$user = $_ENV['USER'];
-$password = $_ENV['PASSWORD'];
-$database = $_ENV['DATABASE'];
-$port = $_ENV['DB_PORT'];
+  $host = $_ENV['HOST'];
+  $user = $_ENV['USER'];
+  $password = $_ENV['PASSWORD'];
+  $database = $_ENV['DATABASE'];
+  $port = $_ENV['DB_PORT'];
+} else {
+  $host = getenv('HOST');
+  $user = getenv('USER');
+  $password = getenv('PASSWORD');
+  $database = getenv('DATABASE');
+  $port = getenv('DB_PORT');
+}
 
 $dsn = "mysql:host=$host;port=$port;dbname=$database;charset=utf8mb4";
 $pdo = new PDO($dsn, $user, $password);
